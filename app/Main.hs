75812939874eec050cc0be72405f510be524980e
Matt Revelle <matthew.revelle@montana.edu>
2023-09-26 21:49:49 -0600

    Added playerThree action.

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,21 @@ module Main where
 import Learn2State
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let playerOne = Player "one"
+      playerTwo = Player "two"
+      playerThree = Player "three"
+
+  (_, endState) <- runGame
+    ( addPlayer playerOne
+        >> addPlayer playerTwo
+        >> addPlayer playerThree
+        >> setScore playerTwo 100
+        >> attack playerOne playerTwo
+        >> setScore playerOne 200
+        >> attack playerOne playerTwo
+        >> attack playerThree playerOne
+    )
+    initialState
+
+  print endState
